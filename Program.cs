39135d083f75206d2c7139ad6e4116b0e23035a1
SilerHofe <147610277+SilerHofe@users.noreply.github.com>
2024-09-06 22:35:27 +0300

    Update Program.cs
    Убрал лишние отступы
    Заменил название пер-ных на корректные

--- Program.cs
+++ Program.cs
@@ -1,24 +1,24 @@
 string path = "C:/Users/sashs/source/repos/ConsoleApp3/test.txt";
 string? line;
 int n=0;
-int[][] Array = null;
-int[] Vector = null;
+int[][] array = null;
+int[] vector = null;
 
-double MatrixMult(int[][] Array, int[] Vector,int n)
+double MatrixMult(int[][] array, int[] vector,int n)
 {
-    int[] Vector_Tmp=new int[n];
+    int[] vector_tmp=new int[n];
     for(int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            Vector_Tmp[i] += Array[j][i]*Vector[j];
-    double Result = 0.0;
-    for (int i = 0; i < n; i++) Result += Vector_Tmp[i] * Vector[i];
-    return Result;
+            vector_tmp[i] += array[j][i]*vector[j];
+    double result = 0.0;
+    for (int i = 0; i < n; i++) result += vector_tmp[i] * vector[i];
+    return result;
 }
-bool SimetryCheck(int[][] Array,int n)
+bool SimetryCheck(int[][] array,int n)
 {
     for(int i = 0; i < n; i++)
         for(int j = i; j < n; j++)
-        if (Array[i][j] != Array[j][i]) return false;
+        if (array[i][j] != array[j][i]) return false;
     return true;
 }
 try
@@ -26,37 +26,36 @@ try
     StreamReader sr = new StreamReader(path);
     line = sr.ReadLine();
     n =Convert.ToInt32(line);
-    Array = new int[n][];
-    Vector = new int[n];
+    array = new int[n][];
+    vector = new int[n];
     for (int i = 0; i < n; i++)
     {
         line = sr.ReadLine();
-        Array[i] = line.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+        array[i] = line.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
     }
     line = sr.ReadLine();
-    Vector=line.Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+    vector=line.Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
     
     sr.Close();
 } catch (Exception e)
 {
     Console.Write("Exception: "+e.Message);
 }
-    Console.WriteLine(n);
-    for (int i = 0; i < n; i++)
+Console.WriteLine(n);
+for (int i = 0; i < n; i++)
+{
+    for(int j = 0; j < n; j++)
     {
-        for(int j = 0; j < n; j++)
-        {
-            Console.Write($"{Array[i][j]}\t");
-        }
-        Console.WriteLine();
+        Console.Write($"{array[i][j]}\t");
     }
-    for (int i = 0; i < n; i++)
-    {
-        Console.WriteLine($"{Vector[i]}\t");
-    }
-    
-    if (SimetryCheck(Array, n))
-    {
-        double Result = MatrixMult(Array, Vector, n);
-        Console.WriteLine(Math.Sqrt(Result));
-    }else Console.WriteLine("Matrix not simetry!");
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"{vector[i]}\t");
+}
+if (SimetryCheck(array, n))
+{
+    double result = MatrixMult(array, vector, n);
+    Console.WriteLine(Math.Sqrt(result));
+}else Console.WriteLine("Matrix not simetry!");
