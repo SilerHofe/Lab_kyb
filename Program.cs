e9926326b626414193a2f7ae65b9f994c483f5e0
SilerHofe <147610277+SilerHofe@users.noreply.github.com>
2024-09-09 14:07:37 +0300

    Add files via upload
    2 задача

--- Program.cs
+++ Program.cs
@@ -1,61 +1,169 @@
-string path = "C:/Users/sashs/source/repos/ConsoleApp3/test.txt";
-string? line;
-int n=0;
-int[][] array = null;
-int[] vector = null;
+﻿using System.Net.Http.Headers;
+using System.Numerics;
+namespace KSAD_Lab2
+{
+    internal class Complex
+    {
+        private double real, imaginary;
+        public double Real { get { return real; } set { real = value; } }
+        public double Imaginary { get { return imaginary; } set { imaginary = value; } }
+        public Complex(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+        public static Complex operator +(Complex x, Complex y)
+        {
+            return new Complex(x.real + y.real, x.imaginary + y.imaginary);
+        }
+        public static Complex operator -(Complex x, Complex y)
+        {
+            return new Complex(x.real - y.real, x.imaginary - y.imaginary);
+        }
+        public static Complex operator *(Complex x, Complex y)
+        {
+            return new Complex(x.real*y.real-x.imaginary*y.imaginary,x.real*y.imaginary+x.imaginary*y.real);
+        }
+        public static Complex operator /(Complex x,Complex y)
+        {
+            double denominator = y.real * y.real + y.imaginary * y.imaginary;
+            return new Complex((x.real * y.real + x.imaginary * y.imaginary) / denominator, (x.imaginary*y.real-x.real*y.imaginary)/denominator);
 
-double MatrixMult(int[][] array, int[] vector,int n)
-{
-    int[] vector_tmp=new int[n];
-    for(int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            vector_tmp[i] += array[j][i]*vector[j];
-    double result = 0.0;
-    for (int i = 0; i < n; i++) result += vector_tmp[i] * vector[i];
-    return result;
-}
-bool SimetryCheck(int[][] array,int n)
-{
-    for(int i = 0; i < n; i++)
-        for(int j = i; j < n; j++)
-        if (array[i][j] != array[j][i]) return false;
-    return true;
-}
-try
-{
-    StreamReader sr = new StreamReader(path);
-    line = sr.ReadLine();
-    n =Convert.ToInt32(line);
-    array = new int[n][];
-    vector = new int[n];
-    for (int i = 0; i < n; i++)
-    {
-        line = sr.ReadLine();
-        array[i] = line.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+        }
+        public static double Abs(Complex x)
+        {
+            return Math.Sqrt(x.real * x.real + x.imaginary * x.imaginary);
+        }
+        public static double Argument(Complex x)
+        {
+            return Math.Atan(x.imaginary/ x.real);
+        }
+
+        public static void Print(Complex x)
+        {
+            if (x.imaginary >= 0) Console.WriteLine($"{x.real}+{x.imaginary}i");
+            else Console.WriteLine($"{x.real}-{Math.Abs(x.imaginary)}i");
+        }
     }
-    line = sr.ReadLine();
-    vector=line.Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
-    
-    sr.Close();
-} catch (Exception e)
-{
-    Console.Write("Exception: "+e.Message);
-}
-Console.WriteLine(n);
-for (int i = 0; i < n; i++)
-{
-    for(int j = 0; j < n; j++)
+    class Program
     {
-        Console.Write($"{array[i][j]}\t");
+        static void Main()
+        {
+            bool f = true;
+            Complex x = new Complex(0, 0);
+            Complex y = new Complex(0, 0);
+            while (f)
+            {
+                Console.WriteLine("Введите 0 для создания комплексного числа");
+                Console.WriteLine("Введиет 1 для сложения");
+                Console.WriteLine("Введите 2 для вычитания");
+                Console.WriteLine("Введите 3 для произведения");
+                Console.WriteLine("Введите 4 для деления");
+                Console.WriteLine("Введите 5 для вычисления модуля");
+                Console.WriteLine("Введите 6 для нахождения аргумента");
+                Console.WriteLine("Введите q/Q для выхода");
+                char enter;
+                enter = Convert.ToChar(Console.ReadLine());
+                switch (enter) 
+                {
+                    case '0':
+                        Console.WriteLine("Введите вещественную часть числа");
+                        double real1=Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите мнимую часть числа");
+                        double imaginary1 = Convert.ToDouble(Console.ReadLine());
+                        x = new Complex(real1, imaginary1);
+
+                        continue;
+                    case '1':
+                        Console.WriteLine("Введите вещественную часть числа");
+                        double real2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите мнимую часть числа");
+                        double imaginary2 = Convert.ToDouble(Console.ReadLine());
+                        y = new Complex(real2, imaginary2);
+                        Complex sum = x + y;
+                        Complex.Print(sum);
+                        continue;
+                    case '2':
+                        Console.WriteLine("Введите вещественную часть числа");
+                        real2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите мнимую часть числа");
+                        imaginary2 = Convert.ToDouble(Console.ReadLine());
+                        y = new Complex(real2, imaginary2);
+                        Complex dif = x - y;
+                        Complex.Print(dif);
+                        continue;
+                    case '3':
+                        Console.WriteLine("Введите вещественную часть числа");
+                        real2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите мнимую часть числа");
+                        imaginary2 = Convert.ToDouble(Console.ReadLine());
+                        y = new Complex(real2, imaginary2);
+                        Complex mult = x * y;
+                        Complex.Print(mult);
+                        continue;
+                    case '4':
+                        Console.WriteLine("Введите вещественную часть числа");
+                        real2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите мнимую часть числа");
+                        imaginary2 = Convert.ToDouble(Console.ReadLine());
+                        y = new Complex(real2, imaginary2);
+                        Complex del = x / y;
+                        Complex.Print(del);
+                        continue;
+                    case '5':
+                        Console.WriteLine("Если хотите найти модуль введенного числа введите 1 если хотитее ввести новое введите 2");
+                        char enter1;
+                        enter1=Convert.ToChar(Console.ReadLine());
+                        switch (enter1)
+                        {
+                            case '1':
+                                double abs = Complex.Abs(x);
+                                Console.WriteLine(abs);
+                                break;
+                            case '2':
+                                Console.WriteLine("Введите вещественную часть числа");
+                                real2 = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine("Введите мнимую часть числа");
+                                imaginary2 = Convert.ToDouble(Console.ReadLine());
+                                y = new Complex(real2, imaginary2);
+                                abs = Complex.Abs(y);
+                                Console.WriteLine(abs);
+                                break;
+                        }
+                        continue;
+                    case '6':
+                        Console.WriteLine("Если хотите найти аргумент введенного числа введите 1 если хотите ввести новое введите 2");
+                        enter1 = Convert.ToChar(Console.ReadLine());
+                        switch (enter1)
+                        {
+                            case '1':
+                                double arg = Complex.Argument(x);
+                                Console.WriteLine(arg);
+                                break;
+                            case '2':
+                                Console.WriteLine("Введите вещественную часть числа");
+                                real2 = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine("Введите мнимую часть числа");
+                                imaginary2 = Convert.ToDouble(Console.ReadLine());
+                                y = new Complex(real2, imaginary2);
+                                arg = Complex.Argument(y);
+                                Console.WriteLine(arg);
+                                break;
+                        }
+                        continue;
+                    case 'q':
+                        f=false;
+                        break;
+                    case 'Q':
+                        f = false;
+                        break;
+
+
+                    default: 
+                        Console.WriteLine("Неизвестная команда"); 
+                        continue;
+                }
+            }
+        }
     }
-    Console.WriteLine();
 }
-for (int i = 0; i < n; i++)
-{
-    Console.WriteLine($"{vector[i]}\t");
-}
-if (SimetryCheck(array, n))
-{
-    double result = MatrixMult(array, vector, n);
-    Console.WriteLine(Math.Sqrt(result));
-}else Console.WriteLine("Matrix not simetry!");
