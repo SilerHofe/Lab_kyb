36c4180aecb68009866ea6169f8fb24856c04dcd
SilerHofe <147610277+SilerHofe@users.noreply.github.com>
2024-09-23 20:28:32 +0300

    Add files via upload
    8 задача

---
+++ Lab_8/Lab_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyStack;
+class program
+{
+    static void Main(string[] args)
+    {
+        MyStack<int> stack = new MyStack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+        stack.StekPrint();
+        Console.WriteLine(stack.Peek());
+        stack.Pop();
+        Console.WriteLine(stack.Empty());
+        Console.WriteLine(stack.Search(44));
+    }
+}
---
+++ Lab_8/MyStack/MyStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyVector;
+
+namespace MyStack
+{
+    public class MyStack<T> : MyVector<T>
+    {
+        private MyVector<T> stack;
+        private int top;
+        public MyStack()
+        {
+            top = -1;
+            stack = new MyVector<T>();
+        }
+        public void Push(T element)
+        {
+            top++;
+            stack.Add(element);
+        }
+        public T Pop()
+        {
+            T tmp = stack.Element(top);
+            stack.Remove(stack.LastElemnet());
+            top--;
+            return tmp;
+        }
+        public T Peek()
+        {
+            if (top == -1) throw new Exception("Стэк пуст");
+            return stack.Get(top);
+        }
+        public bool Empty()
+        {
+            return stack.IsEmpty();
+        }
+        public int Search(T element)
+        {
+            for (int i = top - 1, j = 1; i >= 0; i--, j++)
+            {
+                if (stack.Element(i).Equals(element)) return j;
+            }
+            return -1;
+        }
+        public void StekPrint()
+        {
+            for (var i = top; i >= 0; i--)
+            {
+                Console.Write($"{stack.Element(i)} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
