08d6846dc8c74eb6e4718e4bd1e3903a8d28dc1d
SilerHofe <147610277+SilerHofe@users.noreply.github.com>
2024-10-03 20:24:53 +0300

    Add files via upload
    Убрал static

---
+++ Lab_9/Lab_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyStack;
+using RPN;
+
+namespace Lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rpn rpn = new rpn();
+            while (true)
+            {
+                Console.WriteLine("Введите выражение: ");
+                Console.WriteLine(rpn.Calculate(Console.ReadLine()));
+            }
+        }
+    }
+}
