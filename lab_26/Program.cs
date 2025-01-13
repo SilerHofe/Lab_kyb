e0e69b85ed210d1b2fc086dc07a5dc18bdee6453
SilerHofe <147610277+SilerHofe@users.noreply.github.com>
2025-01-13 18:51:29 +0300

    26 задача

---
+++ lab_26/Program.cs
@@ -0,0 +1,34 @@
+﻿using MyLibr;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace Task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "input.txt";
+            if (path == null) throw new Exception("path is null");
+            MyHashSet<string> set = new MyHashSet<string>();
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                string? line = sr.ReadLine();
+                while (line != null)
+                {
+                    string?[] words = line.Split(' ');
+                    foreach (string word in words)
+                    {
+                        if (word == null || word == " ") continue;
+                        set.Add(word.ToLower());
+                    }
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception ex) { Console.WriteLine("Exeption: " + ex.Message); };
+            foreach (string word in set.ToArray()) Console.WriteLine(word);
+        }
+    }
+}
