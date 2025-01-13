c80e8573c9079993e2c21bdccaf3e77317e5969e
SilerHofe <147610277+SilerHofe@users.noreply.github.com>
2025-01-13 18:47:29 +0300

    23 задача

---
+++ lab_23/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyLibr;
+
+public class Project
+{
+    static void Main(string[] args)
+    {
+        MyHashSet<string> hashSet = new MyHashSet<string>();
+        string[] array = { "Pampilus", "Bi-2", "Nautilus"};
+        hashSet.AddAll(array);
+        hashSet.Remove("Bi-2");
+        
+        Console.Write(hashSet);
+    }
+}
