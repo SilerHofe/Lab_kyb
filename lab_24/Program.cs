e2e43e09eab3502ee7622af3c843fb506ee8dc72
SilerHofe <147610277+SilerHofe@users.noreply.github.com>
2025-01-13 18:48:50 +0300

    24 задача

---
+++ lab_24/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyLibr;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyTreeSet<int> tree=new MyTreeSet<int>();
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        tree.Add(array);
+        int[] array2 = { 2, 1, 12, 18, 76 };
+        tree.Print();
+        Console.WriteLine("--------------------");
+        tree.Remove(10);
+        tree.Print();
+    }
+}
